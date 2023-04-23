12c61eee86eba71c2e508818436492c56d8abc39
Vova <vkarchevskyi10@gmail.com>
2023-04-23 08:42:33 +0300

    Add AI medium difficult

--- Tic-Tac-Toe/game.js
+++ Tic-Tac-Toe/game.js
@@ -15,15 +15,15 @@ class Board {
         console.table(this._board);
     }
 
-    checkBoard(char) {
-        return (this._board[0][0] == char && this._board[1][0] == char && this._board[2][0] == char) ||
-            (this._board[0][1] == char && this._board[1][1] == char && this._board[2][1] == char) ||
-            (this._board[0][2] == char && this._board[1][2] == char && this._board[2][2] == char) ||
-            (this._board[0][0] == char && this._board[0][1] == char && this._board[0][2] == char) ||
-            (this._board[1][0] == char && this._board[1][1] == char && this._board[1][2] == char) ||
-            (this._board[2][0] == char && this._board[2][1] == char && this._board[2][2] == char) ||
-            (this._board[0][0] == char && this._board[1][1] == char && this._board[2][2] == char) ||
-            (this._board[0][2] == char && this._board[1][1] == char && this._board[2][0] == char);
+    checkBoard(char, board = this._board) {
+        return (board[0][0] == char && board[1][0] == char && board[2][0] == char) ||
+            (board[0][1] == char && board[1][1] == char && board[2][1] == char) ||
+            (board[0][2] == char && board[1][2] == char && board[2][2] == char) ||
+            (board[0][0] == char && board[0][1] == char && board[0][2] == char) ||
+            (board[1][0] == char && board[1][1] == char && board[1][2] == char) ||
+            (board[2][0] == char && board[2][1] == char && board[2][2] == char) ||
+            (board[0][0] == char && board[1][1] == char && board[2][2] == char) ||
+            (board[0][2] == char && board[1][1] == char && board[2][0] == char);
     }
 
     getFreeCells() {
@@ -37,9 +37,15 @@ class Board {
         }
         return cellsId;
     }
+
+    getBoard() {
+        return JSON.parse(JSON.stringify(this._board));
+    }
 }
 
 class AIeasy {
+    constructor() { }
+
     makeMove() {
         const freeCells = board.getFreeCells();
         const randomId = Math.floor(Math.random() * freeCells.length);
@@ -47,9 +53,48 @@ class AIeasy {
     }
 }
 
-class AImedium {
-    makeMove() {
+class AImedium extends AIeasy {
+    _char;
+    _enemyChar;
 
+    constructor(char) {
+        super();
+        this._char = char;
+        this._enemyChar = char == 'x' ? 'o' : 'x';
+    }
+
+    makeMove() {
+        const findPos = (char) => {
+            const currentBoard = board.getBoard();
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 3; j++) {
+                    if (currentBoard[i][j] == ' ') {
+                        currentBoard[i][j] = char; // temporary change symbol
+                        if (board.checkBoard(char, currentBoard)) {
+                            currentBoard[i][j] = ' '; // return to the initial symbol
+                            return i * 3 + j; // if any side can win by doing this move, return this coordinate
+                        }
+                        currentBoard[i][j] = ' '; // return to the initial symbol
+                    }
+                }
+            }
+        }
+
+        const winPos = findPos(this._char);
+        if (winPos !== undefined) {
+            getMove(winPos);
+            return;
+        }
+
+        // search the move to prevent a loss
+        const losePos = findPos(this._enemyChar);
+        if (losePos !== undefined) {
+            getMove(losePos);
+            return;
+        }
+
+        // In other case make a random move
+        super.makeMove();
     }
 }
 
@@ -75,15 +120,19 @@ function getMove(id) {
         document.getElementById('winner').innerHTML = "O Wins!";
         isStart = false;
         return;
+    } else if (currentMove + 1 == 9) {
+        document.getElementById('winner').innerHTML = "Draw";
+        isStart = false;
+        return;
     }
 
     currentMove++;
 
     if (currentMove % 2 == 0 && player1 != 'User') {
-        setTimeout(player1.makeMove, 500);
+        setTimeout(() => { player1.makeMove() }, 300);
     }
     if (currentMove % 2 == 1 && player2 != 'User') {
-        setTimeout(player2.makeMove, 500);
+        setTimeout(() => { player2.makeMove() }, 300);
     }
 }
 
@@ -110,7 +159,7 @@ function startGame() {
             player1 = new AIeasy();
             break;
         case 'AImedium':
-            player1 = new AImedium();
+            player1 = new AImedium('x');
             break;
         case 'AIhard':
             player1 = new AIhard();
@@ -121,7 +170,7 @@ function startGame() {
             player2 = new AIeasy();
             break;
         case 'AImedium':
-            player2 = new AImedium();
+            player2 = new AImedium('o');
             break;
         case 'AIhard':
             player2 = new AIhard();
