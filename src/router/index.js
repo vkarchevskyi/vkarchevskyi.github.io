cfc30fb486fe896b219ae961c1ee83d6f5b293a9
Volodymyr Karchevskyi <vkarchevskyi10@gmail.com>
2024-01-21 20:30:18 +0200

    Fix: vue router.

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import MainPage from '../views/MainPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  base: '/',
+  mode: 'history',
   routes: [
     {
       path: '/',
